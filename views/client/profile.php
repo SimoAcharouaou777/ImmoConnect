ba87ac42254884a39372fde9c8c18adf204ba64a
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2023-12-29 12:16:57 +0100

    styling user profile

--- views/client/profile.php
+++ views/client/profile.php
@@ -1,12 +1,12 @@
-<?php require_once '../partials/navbar.php'; ?>
+<?php require_once '../partials/navbar.php';?>
 
 
 
-<aside id="logo-sidebar" class="fixed top-25 left-0 z-40 w-80 transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-  
+<aside id="logo-sidebar1" class=" fixed top-25 left-0 z-40 w-80 " aria-label="Sidebar">
+
 <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-   
-    <div class="flex flex-col items-center p-10">
+
+    <div class="flex flex-col items-center text-center p-5">
         <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="../../public/imgs/about-us-testimonials1-83x83 1.png" alt="Bonnie image"/>
         <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Rachel Gray</h5>
         <span class="text-sm text-gray-500 dark:text-gray-400">Seller</span>
@@ -14,24 +14,9 @@
          <!-- when you come back do this , add informations -->
          <h5 >PHONE : +2125489484848</h5>
          <h5 >EMAIL: example@gmail.com</h5>
-        </div>
-    </div>
-</div>
-
-</aside>
-
-<aside id="logo-sidebar" class="fixed top-25 left-0 z-40 w-80 transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-  
-<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-   
-    <div class="flex flex-col items-center p-10">
-        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="../../public/imgs/about-us-testimonials1-83x83 1.png" alt="Bonnie image"/>
-        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Rachel Gray</h5>
-        <span class="text-sm text-gray-500 dark:text-gray-400">Seller</span>
-        <div class="grid mt-4 md:mt-6">
-         <!-- when you come back do this , add informations -->
-         <h5 >PHONE : +2125489484848</h5>
-         <h5 >EMAIL: example@gmail.com</h5>
+         <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white p-4">Fell Free To Write Me A Message</label>
+        <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+         <button type="button" class="bgColor2 text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mt-2">Send</button>
         </div>
     </div>
 </div>
@@ -62,7 +47,7 @@
                     <span><i class="fa-solid fa-bed"></i> 4</span>
                 </div>
             </div>
-        
+
             <div class="max-w-sm bgColor1 border shadow-lg border-gray-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                 <a href="#">
                     <img class="rounded-t-lg" src="/immoconnect/public/imgs/img5.jpg" alt="" />
@@ -105,7 +90,7 @@
         <div>
             <button type="button" class="text-white bgColor2 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-8 py-4 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Browse More Properties</button>
         </div>
-        
+
     </div>
     </section>
 </div>
