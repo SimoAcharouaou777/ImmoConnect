a6d254f5809f585484109d97f6a5f935cbf7bfce
abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2023-12-29 11:32:40 +0100

    ona terminer la page de login

--- views/auth/login.php
+++ views/auth/login.php
@@ -23,7 +23,23 @@
         font-family: 'Roboto', sans-serif;
      
     }
-   
+    .pageLogin{
+       background-image: url('../../Public/imgs/login.jpg'); 
+       background-size: cover;
+       height: 93vh;
+       border: 0px solid var(--color4, #000);
+        /* backdrop-filter: blur(2px); */
+        flex-shrink: 0;
+        backdrop-filter: blur(4px);
+
+    }
+    .pageLogin form{
+      background-color: #0003;
+    }
+   i{
+    color :#000;
+    
+}
     
 </style>
 </head>
@@ -31,41 +47,33 @@
 
 
 <body>
-
-<section class="bg-center h-full bg-[url('../../public/imgs/login.jpg')] ">
-
-  <div class="  bg-gray-400	opacity-80 h-full	shadow-2xl w-1/4 rounded-lg shadow sm:p-6  md:p-8  mt-36 mx-auto  " id="pageLogin"  >
-    <form class="space-y-6     mx-auto" action="#">
-        <h3 class="text-5xl font-bold text-center  leading-9 ">LOGIN FORM</h3>
-        <div>
-            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+<div class="pageLogin md:p-10 pt-20 m-8 ">
+  <form class="lg:w-1/4   md:w-2/4 w-full  mx-auto block  px-14 p-10  h-fit " action="#">
+        <h3 class="text-5xl font-bold text-center  leading-9  ">LOGIN FORM</h3>
+        <div class="mt-10">
+            <label for="email" class="block mb-2 text-stone-50 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
             <input type="email" name="email" id="email" class="bg-gray-50 border w-64	h-14	 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@exemple.com" required>
         </div>
-        <div>
-            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border w-64	h-14	  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-600 dark:border-gray-500 w-full dark:placeholder-gray-400 dark:text-white" required>
+        <div class="mt-10">
+            <label for="password" class="block mb-2 text-stone-50 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border w-64	h-14   border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-600 dark:border-gray-500 w-full dark:placeholder-gray-400 dark:text-white" required>
         </div>
        
-        <button type="submit" class="w-full text-white bg-blue-950	 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
-        <div class="text-center font-medium mx-auto block  text-gray-500 dark:text-gray-300">
-        <i class="fa-brands fa-x-twitter text-3xl "></i>
+        <button type="submit" class="w-full md:mt-10   text-white bg-blue-950 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
+        <div class="text-center md:mt-10 font-medium mx-auto block  text-gray-500 dark:text-gray-300">
+        <i class="fa-brands fa-x-twitter  text-3xl "></i>
         <i class="fa-brands fa-facebook text-3xl "></i>
         <i class="fa-brands fa-square-instagram text-3xl "></i>
         </div>
 </form>
+
 </div>
-</section>
+
 
 
 
 
   
-    
-    
-
-
-
-
 
 </body>
 </html>
