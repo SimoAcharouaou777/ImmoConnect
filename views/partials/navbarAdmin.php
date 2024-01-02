2d5abbfb894ead3e45f0e05c4b8360737cbefe28
fihar abdelali <fiharali0@gmail.com>
2024-01-02 14:14:33 +0100

    add some changes

--- views/partials/navbarAdmin.php
+++ views/partials/navbarAdmin.php
@@ -122,12 +122,8 @@
                 </li>
                 <li>
                     <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:border border-sky-500 ease-in duration-300 group">
-                    <lord-icon
-    src="https://cdn.lordicon.com/heexevev.json"
-    trigger="loop"
-    delay="4000"
-    style="width:30px;height:30px">
-</lord-icon>
+                        <lord-icon src="https://cdn.lordicon.com/heexevev.json" trigger="loop" delay="4000" style="width:30px;height:30px">
+                        </lord-icon>
                         <span class="ms-3  font-bold">Reservations</span>
                     </a>
                 </li>
